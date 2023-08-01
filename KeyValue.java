2ca954004a99804250b8b3f44cdaf3f9607648c4
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-01 12:00:58 +0530

    Create KeyValue.java

---
+++ KeyValue.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+public class putfunction{
+    public static void main(String[] args) {
+        HashMap<String,Integer>map=new HashMap<>();
+        // creation
+        map.put("India",123);
+        map.put("China",123);
+        System.out.println(map);
+
+}
+
+
+    
+}
